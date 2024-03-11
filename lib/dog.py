71810794266c0bf6dedc7a8acbad03b5950d2042
Emilymatoke <emilymomata@gmail.com>
2024-03-11 13:20:33 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,14 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name) :
+        self.name = name 
+
+    def get_name(self) :
+        print("str")
+        return self._name
+    
+    
+
+
+        
